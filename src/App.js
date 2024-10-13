afc79c65ddb60c58f71bcc89b561147c7ca47a63
Kashif Mohammed <kashif.i.mohammed@gmail.com>
2024-10-13 16:37:05 -0500

    replaced with hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import LeftNav from './components/LeftNav';
 import Workspace from './components/Workspace';
