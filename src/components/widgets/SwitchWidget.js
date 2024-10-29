a9fac31b1211313651e0514b4bcff7e837e7797d
Kashif Mohammed <kashif.i.mohammed@gmail.com>
2024-10-29 06:16:12 -0500

    updated form widget styles for text area and switch widget

--- src/components/widgets/SwitchWidget.js
+++ src/components/widgets/SwitchWidget.js
@@ -3,7 +3,7 @@ import { IgrSwitch } from 'igniteui-react';
 
 const SwitchWidget = ({ label, name, checked, onChange, width }) => (
   <div className={`flex items-center ${width || 'w-full'} p-2`}>
-    <label className="text-sm text-gray-600 mr-4">{label}</label>
+    <label className="text-sm  mr-4">{label}</label>
     <IgrSwitch checked={checked} onCheckedChanged={(e) => onChange(e, name)} />
   </div>
 );
